e36dd2fee86a08bab37cb0a0d466b9b8d9aa6e77
UnnatiBhal <109412356+UnnatiBhal@users.noreply.github.com>
2022-12-06 23:46:06 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e4c9655b4emshc2f0779032ad318p1eaa3cjsn1e647ce9c88b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ebed3b5d4ec4f4f181b1840d73bf5ffb"; 
+
